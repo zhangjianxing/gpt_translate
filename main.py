21f4733931f19e726db5d67566896f2a6b464041
jxzhang <jxzhang@wish.com>
2023-12-09 19:00:02 +0800

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+
+from ai_translator.utils import ArgumentParser, ConfigLoader
+from ai_translator.model import OpenAIModel
+from ai_translator.translator import PDFTranslator
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+if __name__ == "__main__":
+    argument_parser = ArgumentParser()
+    args = argument_parser.parse_arguments()
+    config_loader = ConfigLoader(args.config)
+
+    config = config_loader.load_config()
+
+    model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
+    api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
+    model = OpenAIModel(model=model_name, api_key=api_key)
+
+    pdf_file_path = args.book if args.book else config['common']['book']
+    file_format = args.file_format if args.file_format else config['common']['file_format']
+
+    # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
+    translator = PDFTranslator(model)
+    translator.translate_pdf(pdf_file_path, file_format)
